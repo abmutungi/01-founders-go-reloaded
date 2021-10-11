2c6b5c275ee6011aa6988a71b6b493bb2cc763f6
abmutungi <abmutungi@gmail.com>
2021-10-11 18:24:37 +0100

    go reloaded

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package main
+/*package main
 
 import (
 	"fmt"
@@ -12,4 +12,49 @@ func main() {
 		return
 	}
 	fmt.Println(string(data))
+}
+*/
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	// readWordFromStandardInput()
+	// readLineFromStandardInput()
+	// readFromFile()
+}
+
+func readWordFromStandardInput() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
+	scanner.Scan()
+	fmt.Println(scanner.Text())
+}
+
+func readLineFromStandardInput() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	fmt.Println(scanner.Text())
+}
+
+func readFromFile() {
+	file, err := os.Open("testing.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	fileScanner := bufio.NewScanner(file)
+	for fileScanner.Scan() {
+		fmt.Println(fileScanner.Text())
+	}
+
+	if err := fileScanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
