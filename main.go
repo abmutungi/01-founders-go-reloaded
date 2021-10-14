96ff1c44b237d4ce8467a5a2ea07c150bce7cef2
abmutungi <abmutungi@gmail.com>
2021-10-14 18:29:40 +0100

    f

--- main.go
+++ main.go
@@ -2,35 +2,45 @@ package main
 
 import (
 	"fmt"
-	"strings"
 	"os"
-	"strconv"
+	"strings"
 )
 
 func main() {
-	
 	arguments := os.Args[1:]
 
- 	// error handling
+	// error handling
 
-	// if len(arguments) < 1 {
-    //     fmt.Println("Please give one argument.")
-	// }
-
-	// if len(arguments) > 2 {
-	// 	fmt.Println("Too many arguments")
-	// }
-
-	//read input file
-
-	text,_ := os.ReadFile(arguments[0])
-
-	word := strings.Split(text, " ")
-
-
-	for i, word := range word {
-		if word == "(up)"{
-		word[i-1] = strings.ToUpper(i-1)
-		}
-
+	if len(arguments) < 1 {
+		fmt.Println("Please give one argument.")
 	}
+
+	if len(arguments) > 2 {
+		fmt.Println("Too many arguments")
+	}
+
+	// read input file
+
+	story, _ := os.ReadFile(arguments[0])
+
+	text := strings.Split(string(story), " ")
+
+	for i, word := range text {
+		switch word {
+		case word == "(up)":
+			text[i-1] = strings.ToUpper(text[i-1])
+			text = append(text[:i], text[i+1:]...)
+
+		case word == "(low)":
+			text[i-1] = strings.ToLower(text[i-1])
+			text = append(text[:i], text[i+1:]...)
+
+		case word == "(cap)":
+			text[i-1] = strings.ToTitle(text[i-1])
+			text = append(text[:i], text[i+1:]...)
+
+		default:
+			fmt.Println(text)
+		}
+	}
+}
