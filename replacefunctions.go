322c74c70c3141e19c3660abe7c367f36cd11668
abmutungi <abmutungi@gmail.com>
2021-10-18 19:00:14 +0100

    2

--- replacefunctions.go
+++ replacefunctions.go
@@ -7,21 +7,24 @@ import (
 )
 
 func ToHex(s string) string {
-	sliceOfS := strings.Split(s, " ")
+	sliceOfStr := strings.Split(s, " ")
 	var emptySlice string
 
 	for a := len(sliceOfS) - 1; a >= 0; a-- {
-		if sliceOfS[a] != "(hex)" {
-			emptySlice += sliceOfS[a]
+		if sliceOfStr[a] != "(hex)" {
+			emptySlice += sliceOfStr[a]
 			fmt.Print(emptySlice)
-		} else if sliceOfS[a] == "(hex)" {
+		} else if sliceOfStr[a] == "(hex)" {
 			a -= 1
-			numberStr := strings.Replace(s, "", -1)
-			numberStr = strings.Replace(numberStr, "", -1)
+			numberStr := strings.Replace(sliceOfStr[a], -1)
+			numberStr = strings.Replace(numberStr, -1)
 
-			output, _ := strconv.ParseInt(toHex(), 16, 64)
+			output, _ := strconv.ParseInt(numberStr, 64)
+			inString := strconv.FormatInt(output, 10)
+			emptySlice = inString + " " + emptySlice
 		}
 	}
+	return emptySlice
 }
 
 /*func toBin(s string) string {
