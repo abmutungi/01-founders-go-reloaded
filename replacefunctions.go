9c053e0cb8fed05b8f9f9781d7f8a4c2f41164d1
abmutungi <abmutungi@gmail.com>
2021-10-19 17:34:03 +0100

    1

--- replacefunctions.go
+++ replacefunctions.go
@@ -1,7 +1,6 @@
 package gr
 
 import (
-	"fmt"
 	"strconv"
 	"strings"
 )
@@ -10,24 +9,69 @@ func ToHex(s string) string {
 	sliceOfStr := strings.Split(s, " ")
 	var emptySlice string
 
-	for a := len(sliceOfS) - 1; a >= 0; a-- {
+	for a := len(sliceOfStr) - 1; a >= 0; a-- {
 		if sliceOfStr[a] != "(hex)" {
-			emptySlice += sliceOfStr[a]
-			fmt.Print(emptySlice)
+			emptySlice = emptySlice + " " + sliceOfStr[a]
 		} else if sliceOfStr[a] == "(hex)" {
 			a -= 1
-			numberStr := strings.Replace(sliceOfStr[a], -1)
-			numberStr = strings.Replace(numberStr, -1)
+			var replace string
+			numberStr := strings.Replace(sliceOfStr[a], replace, "", -1)
+			numberStr = strings.Replace(numberStr, replace, "", -1)
 
-			output, _ := strconv.ParseInt(numberStr, 64)
+			output, _ := strconv.ParseInt(numberStr, 16, 64)
 			inString := strconv.FormatInt(output, 10)
-			emptySlice = inString + " " + emptySlice
+			emptySlice = emptySlice + " " + inString
 		}
 	}
 	return emptySlice
 }
 
-/*func toBin(s string) string {
+func ToBin(s string) string {
+	sliceOfStr := strings.Split(s, " ")
+	var emptySlice string
 
+	for a := len(sliceOfStr) - 1; a >= 0; a-- {
+		if sliceOfStr[a] != "(bin)" {
+			emptySlice = emptySlice + " " + sliceOfStr[a]
+		} else if sliceOfStr[a] == "(bin)" {
+			a -= 1
+			var replace string
+			numberStr := strings.Replace(sliceOfStr[a], replace, "", -1)
+			numberStr = strings.Replace(numberStr, replace, "", -1)
+
+			output, _ := strconv.ParseInt(numberStr, 2, 64)
+			inString := strconv.FormatInt(output, 10)
+			emptySlice = emptySlice + " " + inString
+		}
+	}
+	return emptySlice
 }
-*/
+
+// func ToUpper(s string) string {
+// 	sliceOfStr := strings.Split(s, " ")
+// 	var emptySlice string
+
+// 	for a := len(sliceOfStr) - 1; a >= 0; a-- {
+// 		if sliceOfStr[a] != "(cap)" {
+// 			emptySlice = emptySlice + " " + sliceOfStr[a]
+// 		} else if sliceOfStr[a] == "(cap)" {
+// 			a -= 1
+// 			sliceOfStr[a] = strings.Title(sliceOfStr[a])
+// 			sliceOfStr = append(sliceOfStr[:a], sliceOfStr[a+1:]...)
+// 			emptySlice = emptySlice + " " + sliceOfStr[a]
+// 		}
+// 	}
+// 	return emptySlice
+// }
+
+// func ToUpper(s string) string {
+// 	sliceOfStr := strings.Split(s, " ")
+
+// 	for i, word := range sliceOfStr {
+// 		if word == "(cap)" {
+// 			sliceOfStr[i-1] = strings.ToTitle(sliceOfStr[i-1])
+// 			sliceOfStr = append(sliceOfStr[:i], sliceOfStr[i+1:]...)
+// 		}
+// 	}
+
+// }
--- test/goreloaded.go
+++ test/goreloaded.go
@@ -8,31 +8,7 @@ import (
 func main() {
 	textfile := gr.ReadFile()
 	convertHex := gr.ToHex(string(textfile))
-	fmt.Println(convertHex)
+	convertBin := gr.ToBin(convertHex)
+	//	capitalise := gr.ToUpper(convertBin)
+	fmt.Println(convertBin)
 }
-
-/*read input file
-
-story, _ := os.ReadFile(arguments[0])
-
-text := strings.Split(string(story), " ")
-
-for i, word := range text {
-	switch word {
-	case word == "(up)":
-		text[i-1] = strings.ToUpper(text[i-1])
-		text = append(text[:i], text[i+1:]...)
-
-	case word == "(low)":
-		text[i-1] = strings.ToLower(text[i-1])
-		text = append(text[:i], text[i+1:]...)
-
-	case word == "(cap)":
-		text[i-1] = strings.ToTitle(text[i-1])
-		text = append(text[:i], text[i+1:]...)
-
-	default:
-		fmt.Println(text)
-	}
-}
-*/
