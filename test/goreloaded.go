81555ae97122eec719ab12df5d5fd917c26cd611
abmutungi <abmutungi@gmail.com>
2021-10-22 19:10:53 +0100

    post outage

--- test/goreloaded.go
+++ test/goreloaded.go
@@ -8,7 +8,8 @@ import (
 
 func main() {
 	// Read the file and convert to slice of string
-	sliceOfStr := strings.Split(gr.ReadFile(), " ")
+	textFile := gr.ReadFile()
+	sliceOfStr := strings.Split(textFile, " ")
 
 	for i, word := range sliceOfStr {
 		switch word {
@@ -32,12 +33,26 @@ func main() {
 		case "A":
 			sliceOfStr[i] = gr.ChangeA(sliceOfStr[i+1])
 			sliceOfStr[i] = strings.Title(strings.ToLower(sliceOfStr[i]))
-		case "(cap,":
-			sliceOfStr[i] = gr.Atoi(sliceOfStr[i+1])
-			sliceOfStr = append(sliceOfStr[:i], sliceOfStr[i+2:]...)
+			// case "(cap,":
+			// 	break
+			// sliceOfStr = append(sliceOfStr[:i], sliceOfStr[i+2:]...)
 			// case "(up,":
 			// case "(low,":
 
+		}
+	}
+
+	for i := 0; i < len(sliceOfStr); i++ {
+		n := 0
+		var newSlice []string
+
+		if sliceOfStr[i] == "(cap," {
+			n = gr.Atoi(sliceOfStr[i+1])
+			fmt.Println(n)
+			for j := 1; j <= n; j++ {
+				sliceOfStr[i-j] = strings.Title(sliceOfStr[i-j])
+				newSlice = append(newSlice, sliceOfStr[i-j])
+			}
 		}
 	}
 	fmt.Println(sliceOfStr)
