2e6fb85be2f76f3400e4e8650d140a2b6e16396b
abmutungi <abmutungi@gmail.com>
2021-10-22 19:11:39 +0100

    post outage

--- functions.go
+++ functions.go
@@ -48,27 +48,12 @@ func ChangeA(s string) string {
 
 func Atoi(s string) int {
 	num := 0                // initalise num as 0
-	result := 1             // initialise result as 1
-	for i, val := range s { // uses for range loop to iterate over s
-		digit := int(val) - 48
-		if digit <= 9 && digit >= 0 {
+	for _, val := range s { // uses for range loop to iterate over s
+		digit := 0
+		if val <= '9' && val >= '0' {
+			digit = int(val - '0')
 			num = num*10 + digit
-		} else if digit == -3 && i == 0 {
-			result = -1
-		} else if digit == -5 && i == 0 {
-			result = 1
-		} else {
-			return 0
 		}
 	}
-	num *= result
 	return num
-}
-
-func HexNumber(s []string) []string {
-	h := 0
-	var toAdd []string
-
-	for i := 0; i < len(s); i++ {
-	}
 }
