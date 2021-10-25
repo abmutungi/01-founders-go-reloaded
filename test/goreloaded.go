90b1257395c55d5eb38f99255241640c2c9c7f1b
abmutungi <abmutungi@gmail.com>
2021-10-25 19:08:04 +0100

    nearly done

--- test/goreloaded.go
+++ test/goreloaded.go
@@ -6,10 +6,48 @@ import (
 	"strings"
 )
 
+func standardiseSpaces(s string) string {
+	return strings.Join(strings.Fields(s), " ")
+}
+
 func main() {
 	// Read the file and convert to slice of string
-	textFile := gr.ReadFile()
-	sliceOfStr := strings.Split(textFile, " ")
+	sliceOfStr := strings.Split(gr.ReadFile(), " ")
+
+	// for loops for multiple modifications of string
+
+	for i := 0; i < len(sliceOfStr); i++ {
+		n := 0
+
+		if sliceOfStr[i] == "(cap," {
+			n = gr.Atoi(sliceOfStr[i+1])
+			for j := 1; j <= n; j++ {
+				sliceOfStr[i-j] = strings.Title(sliceOfStr[i-j])
+			}
+		}
+	}
+	for i := 0; i < len(sliceOfStr); i++ {
+		n := 0
+
+		if sliceOfStr[i] == "(up," {
+			n = gr.Atoi(sliceOfStr[i+1])
+			for j := 1; j <= n; j++ {
+				sliceOfStr[i-j] = strings.ToUpper(sliceOfStr[i-j])
+			}
+		}
+	}
+	for i := 0; i < len(sliceOfStr); i++ {
+		n := 0
+
+		if sliceOfStr[i] == "(low," {
+			n = gr.Atoi(sliceOfStr[i+1])
+			for j := 1; j <= n; j++ {
+				sliceOfStr[i-j] = strings.ToLower(sliceOfStr[i-j])
+			}
+		}
+	}
+
+	// switch case statement for simple modifications of string
 
 	for i, word := range sliceOfStr {
 		switch word {
@@ -33,27 +71,42 @@ func main() {
 		case "A":
 			sliceOfStr[i] = gr.ChangeA(sliceOfStr[i+1])
 			sliceOfStr[i] = strings.Title(strings.ToLower(sliceOfStr[i]))
-			// case "(cap,":
-			// 	break
-			// sliceOfStr = append(sliceOfStr[:i], sliceOfStr[i+2:]...)
-			// case "(up,":
-			// case "(low,":
-
+		case "(cap,":
+			sliceOfStr = append(sliceOfStr[:i], sliceOfStr[i+2:]...)
+		case "(low,":
+			sliceOfStr = append(sliceOfStr[:i], sliceOfStr[i+2:]...)
+		case "(up,":
+			sliceOfStr = append(sliceOfStr[:i], sliceOfStr[i+2:]...)
 		}
 	}
 
-	for i := 0; i < len(sliceOfStr); i++ {
-		n := 0
-		var newSlice []string
+	restring := strings.Join(sliceOfStr, " ")
 
-		if sliceOfStr[i] == "(cap," {
-			n = gr.Atoi(sliceOfStr[i+1])
-			fmt.Println(n)
-			for j := 1; j <= n; j++ {
-				sliceOfStr[i-j] = strings.Title(sliceOfStr[i-j])
-				newSlice = append(newSlice, sliceOfStr[i-j])
-			}
+	char := []rune(restring)
+
+
+
+	for i := 0; i < len(char); i++ {
+		if char[i] == ',' && char[i-1] == ' ' || char[i] == '.' && char[i-1] == ' ' || char[i] == '!' && char[i-1] == ' ' || char[i] == '?' && char[i-1] == ' ' || char[i] == ':' && char[i-1] == ' ' || char[i] == ';' && char[i-1] == ' ' || char[i] == 39 && char[i-1] == 32 {
+			char[i], char[i-1] = char[i-1], char[i]
 		}
 	}
-	fmt.Println(sliceOfStr)
+
+	if char[i] == 39 {
+
+	count := 0
+	
+	for i := 0; i < len(char); i++ {
+
+		if char[i] == 39 && count%2 != 0 && char[i-1] == 32 {
+			char[i], char[i-1] == char[i-1], char[i]
+			count++
+		}
+		if char[i] == 39 && count%2 = 0 && char[i+1] == 32 {
+			char[i], char[i+1] == char[i+1], char[i]
+		}
+	}
+}
+	a := standardiseSpaces(string(char))
+	fmt.Println(a)
 }
